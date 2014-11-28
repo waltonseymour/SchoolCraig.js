2bf0cb65076a0127b7207728b79092e4842103c5
Walton Seymour <bigw369@gmail.com>
2014-11-28 00:55:44 -0500

    added regex matching for uuids

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var models = require('../models');
 var crypto = require('crypto');
 var _ = require('underscore');
+var util = require('../utilities');
 
 var publicOptions = {attributes: ['id', 'fname', 'lname', 'email']};
 
 // Retrieves all users
 router.get('/all', function(req, res) {
-  if (req.session.userID === undefined) { res.send(403); }
+  if (req.session.userID === undefined) { return res.send(403); }
 
   models.User.findAll(publicOptions).success(function (users) {
     res.send(users);
@@ -17,7 +18,8 @@ router.get('/all', function(req, res) {
 
 // Retreives user by id
 router.get('/id/:id', function(req, res) {
-  if (req.session.userID === undefined) { res.send(403); }
+  if (req.session.userID === undefined) { return res.send(403); }
+  if (!util.isUUID(req.params.id)) { return res.send(401); }
 
   var options = _.extend({where: {id: req.params.id}}, publicOptions);
   models.User.find(options).success(function(user){
@@ -32,13 +34,13 @@ router.post('/auth', function(req, res) {
 
   models.User.find({where: {email: email}}).success(function (user) {
     // returns 401 if user does not exist
-    if (!user) { res.send(401); }
+    if (!user) { return res.send(401); }
     var salt = user.salt;
     var DBPassword = user.password;
     password = crypto.createHash('sha256').update(salt + password).digest('hex');
 
     // returns 401 if password is incorrect
-    if (password !== DBPassword) { res.send(401); }
+    if (password !== DBPassword) { return res.send(401); }
     req.session.userID = user.id;
     res.send(200);
   });
@@ -52,7 +54,7 @@ router.post('/deauth', function (req, res) {
 
 // Modifies User by id
 router.put('/id/:id', function(req, res) {
-  if (req.session.userID !== req.params.id) { res.send(403); }
+  if (req.session.userID !== req.params.id) { return res.send(403); }
 
   var user = _.pick(req.body, publicOptions.attributes);
   var options = _.extend({where: {id: req.params.id}}, publicOptions);
@@ -63,7 +65,7 @@ router.put('/id/:id', function(req, res) {
 
 // Deletes User by id
 router.delete('/id/:id', function(req, res) {
-  if (req.session.userID !== req.params.id) { res.send(403); }
+  if (req.session.userID !== req.params.id) { return res.send(403); }
 
   var options = _.extend({where: {id: req.params.id}}, publicOptions);
   models.User.destroy(options).success(function (user) {
@@ -73,7 +75,7 @@ router.delete('/id/:id', function(req, res) {
 
 // Creates new user
 router.post('/', function(req, res) {
-  var user = _.pick(req.body, publicOptions.attributes);
+  var user = req.body;
   if (user.fname && user.lname && user.email && user.password) {
     var salt = crypto.randomBytes(16).toString('hex'); 
     var password = crypto.createHash('sha256').update(salt + user.password).digest('hex');
---
+++ utilities.js
@@ -0,0 +1,6 @@
+module.exports = {
+  isUUID: function (id) {
+    var regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return !!id.match(regex);
+  }
+};
