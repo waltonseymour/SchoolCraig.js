789cd63684daf4fe50d4d07a2126fef13c4ed010
Walton Seymour <bigw369@gmail.com>
2014-12-20 19:42:58 -0500

    supports ajax requests for post creation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,31 @@
 "use strict";
 
 $('.post').click(function(event){
-  // handle displaying post here
+  var $this = $(this);
+  var id = $this.attr('data-id');
 });
 
 
+$('#create-submit').click(function () {
+  var post = {};
+  post.title = $('#create-form .create-title').val();
+  post.description = $('#create-form .create-description').val();
+  post.price = $('#create-form .create-price').val();
+  post.category_id = $('#create-form select').val();
+  createPost(post);
+});
 
 $('#logout').click(logout);
+
+function createPost(post){
+    $.ajax({
+    url: 'posts', // the presigned URL
+    type: 'POST',
+    data: post,
+    success: function() { location.reload(); },
+    error: function(err) { console.log("create post failed");}
+  });
+}
 
 function logout(){
   $.ajax({
