adf41c22c302b8c8afefc3ae6f65d962e3d46958
Walton Seymour <bigw369@gmail.com>
2015-01-28 13:24:34 -0600

    added grunt with uglify and cssmin

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,45 +1,47 @@
-"use strict";
+(function() {
+  "use strict";
 
-$('.dropdown-toggle').dropdown();
-$('.dropdown-menu').click(function (e) {
-  e.stopPropagation();
-});
-
-$('#signup-form').parsley({
-  successClass: 'success',
-  errorClass: 'error'
-});
-
-$('#login-dropdown-menu').parsley({
-  successClass: 'success',
-  errorClass: 'error'
-});
-
-$('#signup-form').submit(signup);
-
-$('#login-dropdown-menu').submit(function(){
-  login();
-  return false;
-});
-
-function signup() {
-  var data = {email: $('#signup-email').val(), password: $('#signup-password').val()};
-  $.ajax({
-    url: 'users',
-    type: 'POST',
-    data: data,
-    success: function() { location.reload(); },
-    error: function(err) { console.log("login failed");}
+  $('.dropdown-toggle').dropdown();
+  $('.dropdown-menu').click(function (e) {
+    e.stopPropagation();
   });
-}
 
-function login(){
-  var data = {email: $('#login-email').val(), password: $('#login-password').val()};
-  $.ajax({
-    url: 'users/auth',
-    type: 'POST',
-    data: data,
-    success: function() { location.reload(); },
-    error: function(err) { console.log("login failed");}
+  $('#signup-form').parsley({
+    successClass: 'success',
+    errorClass: 'error'
   });
-}
+
+  $('#login-dropdown-menu').parsley({
+    successClass: 'success',
+    errorClass: 'error'
+  });
+
+  $('#signup-form').submit(signup);
+
+  $('#login-dropdown-menu').submit(function(){
+    login();
+    return false;
+  });
+
+  function signup() {
+    var data = {email: $('#signup-email').val(), password: $('#signup-password').val()};
+    $.ajax({
+      url: 'users',
+      type: 'POST',
+      data: data,
+      success: function() { location.reload(); },
+      error: function(err) { console.log("login failed");}
+    });
+  }
+
+  function login(){
+    var data = {email: $('#login-email').val(), password: $('#login-password').val()};
+    $.ajax({
+      url: 'users/auth',
+      type: 'POST',
+      data: data,
+      success: function() { location.reload(); },
+      error: function(err) { console.log("login failed");}
+    });
+  }
+}());
