f0fb06690b7598ce4a544211e1116fee21f1bfdf
Walton Seymour <bigw369@gmail.com>
2015-03-16 10:39:58 -0500

    migration bug fix

--- migrations/20150316103152-earthdistance.js
+++ migrations/20150316103152-earthdistance.js
@@ -3,8 +3,8 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
     // add altering commands here, calling 'done' when finished
-    migration.migrator.sequelize.query("create extension if not exists cube").then(function(){
-      migration.migrator.sequelize.query("create extension if not exists earthdistance").then(function(){
+    migration.sequelize.query("create extension if not exists cube").then(function(){
+      migration.sequelize.query("create extension if not exists earthdistance").then(function(){
         done();
       });
     });
