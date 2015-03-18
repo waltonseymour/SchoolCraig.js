5e79fe0e86bc78bc9b976fce0f1182659e4c7ad6
Walton Seymour <bigw369@gmail.com>
2015-03-18 10:48:15 -0500

    displays markers for posts on map

--- controllers/index.js
+++ controllers/index.js
@@ -4,15 +4,12 @@ var categoryController = require('../controllers/categories');
 module.exports = {
   main: function (req, res) {
     if(req.session.userID){
-      postController.listAll(req, res, function (posts) {
-        categoryController.listAll(req, res, function (categories) {
-          res.render('main', {userID: req.session.userID, posts: posts, categories: categories});
-        });
-      }); 
+      categoryController.listAll(req, res, function (categories) {
+        res.render('main', {userID: req.session.userID, categories: categories});
+      });
     }
     else {
       res.render('index');
     }
   }
 };
-
