c692b7af856be09e73b0681934a81b786731ad02
Walton Seymour <bigw369@gmail.com>
2015-04-13 11:08:05 -0500

    changes splash image to add overlay

---
+++ public/javascripts/settings.js
@@ -0,0 +1,33 @@
+(function() {
+  "use strict";
+
+  $('#delete-account').click(function(){
+    var id = $('#user-id').val();
+    swal({
+      title: "Are you sure?",
+      text: "Your account cannot be recovered",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#e74c3c",
+      confirmButtonText: "Yes, I understand",
+      cancelButtonText: "No, cancel",
+      closeOnConfirm: false,
+      closeOnCancel: true
+    },
+    function(isConfirm){
+      if (isConfirm) {
+        swal("Deleted!", "Your account has been deleted.", "success");
+        $.ajax({
+          url: 'users/' + id,
+          type: 'DELETE',
+          success: function() {
+            ga('send', 'event', 'User', 'Delete', id);
+            setTimeout(function(){ location.reload(); }, 1000);
+            },
+          error: function(err) { console.log("delete user failed"); }
+        });
+      }
+    });
+  });
+
+}());
